3371f5f745ff9e85396484632fa662ea2ffe4371
Brad Walker <Bwalker8038@gmail.com>
2011-07-19 13:30:58 -0400

    added config.ru

--- server.rb
+++ server.rb
@@ -1,15 +1,39 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'data_mapper'
 
 configure do
-  set :haml, :format => :html5
+    set :haml, :format => :html5
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+end
+
+class Contact
+    include DataMapper::Resource
+    property :id,    Serial
+    property :name,  String
+    property :email, String
+    property :disc,  Text
 end
 
 get '/' do
-  haml :index
+    haml :index
 end
 
 get '/contact' do
     haml :contact
 end
+
+post '/contact' do
+    @contact = Contact.new(:name  => params[:name], 
+                           :email => params[:email],
+                           :disc  => params[:disc])
+    if @contact.save
+        status 201
+        redirect '/contact/successful'
+    else
+        status 412
+        redirect '/contact'
+    end
+end
+
