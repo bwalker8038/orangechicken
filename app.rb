8bb49024d7709e291db5bbb0a5192548c6434bda
Brad Walker <bwalker8038@gmail.com>
2012-05-28 08:35:53 -0400

    changed css to compass

--- app.rb
+++ app.rb
@@ -1,24 +1,32 @@
+require 'compass'
 require 'sinatra'
 require 'haml'
-require 'datamapper'
+#require 'datamapper'
+
 
 # connect to database
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
+#DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://my.db')
 
 configure do
     set :haml, :format => :html5
+    Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
 end
 
-class Contact
-    include DataMapper::Resource
-    property :id,    Serial
-    property :name,  String
-    property :email, String
-    property :disc,  Text
-end
+#class Contact
+#    include DataMapper::Resource
+#    property :id,    Serial
+#    property :name,  String
+#    property :email, String
+#    property :disc,  Text
+#end
 
 
 # views
+get '/css/:name.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    scss(:"css/#{params[:name]}", Compass.sass_engine_options)
+end
+
 get '/' do
     haml :index
 end
